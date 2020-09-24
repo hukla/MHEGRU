b22acde82551935040007e075acf63c046b5a17d
Jaehee Jang <hukla.jj@gmail.com>
2020-09-24 12:48:00 +0900

    modulized; yet not working for long seq

---
+++ include/MHEGRU.h
@@ -0,0 +1,91 @@
+#ifndef __MHEGRU_H__
+#define __MHEGRU_H__
+
+#include <NTL/ZZ.h>
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <random>
+#include <chrono>
+
+#include "MHEAAN/TimeUtils.h"
+#include "MHEAAN/Ring.h"
+#include "MHEAAN/SecretKey.h"
+#include "MHEAAN/Scheme.h"
+#include "MHEAAN/HERNN.h"
+
+class MHEGRU {
+private:
+
+    // the size of GRU hidden
+    int hiddenSize;
+    // the size of model input
+    int inputSize;
+    // the number of classes to classify (1 for regression); the size of model output
+    int numClass;
+    // the input sequence length
+    int bptt;
+
+    /// AddingProblem model weights
+    // GRU weights for input to hidden
+    double **Wh, **Wr, **Wz;
+    // GRU weights for hidden to hidden
+    double **Uh, **Ur, **Uz;
+    // GRU biases
+    double *bh, *br, *bz;
+    // FC layer weight
+    double** FW;
+    // FC layer bias
+    double* Fb;
+
+    TimeUtils timeutils;
+
+    /// Encryption
+    Ring ring;
+    SecretKey secretKey;
+    Scheme scheme;
+    HERNN hernn;
+
+    long logp = 33;
+
+    /// Ciphertext model weights and intermediate variables
+    Ciphertext enc_Wh, enc_Uh, enc_bh; // lr
+    Ciphertext enc_Wz, enc_Uz, enc_bz;
+    Ciphertext enc_Wr, enc_Ur, enc_br;
+    Ciphertext enc_z, enc_r, enc_g;
+
+    Ciphertext enc_FW, enc_Fb;
+
+
+public:
+    /// default constructor
+    MHEGRU();
+
+    /// class constructor
+    /// \param hiddenSize [in] integer variable that contains the size of GRU hidden
+    /// \param inputSize [in] integer variable that contains the size of model input
+    /// \param numClass [in] integer variable that contains the number of classes to classify (1 for regression); the size of model output
+    /// \param bptt [in] integer variable that contains the input sequence length
+    MHEGRU(int hiddenSize, int inputSize, int numClass, int bptt);
+
+    /// loadWeights loads plaintext weights from file in path
+    /// \param path [in] string variable that contains path for model weight files
+    void loadWeights(string path);
+
+    void encryptWeights();
+
+    void printEncryptedWeights();
+
+    /// forward evaluates model forward propagation
+    /// \param input_path [in] string variable that contains path for input string file
+    void forward(string input_path);
+
+
+};
+
+#endif // __MHEGRU_H__
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-#include "MHEAddingProblem.h"
+#include "MHEGRU.h"
 
-int main() {
+int main(int argc, char **argv) {
     SetNumThreads(16);
-    MHEAddingProblem app = MHEAddingProblem();
-    app.run();
+    std::cout << argc << std::endl;
+    std::cout << argv[1] << std::endl;
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " TASK_ID" << std::endl;
+        std::cerr << "TASK_ID: MNIST=1, addingProblem=2, " << std::endl;
+        return 1;
+    }
+
+    int hiddenSize, inputSize, numClass, bptt;
+    string path;
+
+    if (strcmp(argv[1], "1") == 0)
+    {
+        // MNIST
+        hiddenSize = 64, inputSize = 32, numClass = 16, bptt = 28;
+        path = "/home/hukla/PycharmProjects/cryptoGRU/MNIST_enc/";
+    } 
+    else if (strcmp(argv[1], "2") == 0)
+    {
+        // addingProblem
+        hiddenSize = 64, inputSize = 2, numClass = 1, bptt = 6;
+        path = "/home/hukla/CLionProjects/MHEGRU/addingProblem_6/";
+
+    }
+    else
+    {
+        std::cerr << "Usage: " << argv[0] << " TASK_ID" << std::endl;
+        std::cerr << "TASK_ID: addingProblem=1, MNIST=2" << std::endl;
+        return 1;    
+    }
+    
+
+    MHEGRU *model = new MHEGRU(hiddenSize, inputSize, numClass, bptt);
+    model->loadWeights(path);
+    model->encryptWeights();
+    // model->printEncryptedWeights();
+
+    model->forward(path + "input_0/");
+
+    return 0;
 }
